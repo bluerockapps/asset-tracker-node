f05b07ab8b97a678edf0bd80620644f05cbb1bfc
Reid Fralick <reid.fralick@gmail.com>
2019-09-03 15:42:43 -0600

    updated node to include assetMapSecurity and updated permissions

--- app/modules/security.js
+++ app/modules/security.js
@@ -16,6 +16,22 @@ module.exports = function(app, client, VerifyToken) {
     });
   });
 
+  // Get asset map security
+  app.get('/security/asset/map', VerifyToken, (req, res) => {
+    console.log(req.query)
+    client.query('SELECT asset_map_update, asset_map_read '
+                +'FROM public.permission '
+                +'WHERE tenant_id = ($1) AND role_id = ($2)', 
+                [req.query.tenant_id, req.query.role_id], 
+      (err, respon) => {
+      if (err) {
+        console.log(err)
+        res.status(500).send('Failed to return security roles.') }
+      else
+        res.status(200).send(respon.rows[0]);
+    });
+  });
+
   // Get asset security
   app.get('/security/asset', VerifyToken, (req, res) => {
     client.query('SELECT asset_create, asset_update, asset_delete '
