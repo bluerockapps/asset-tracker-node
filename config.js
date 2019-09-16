3ad5779872d14e0c508bac4bb0cc53bfa59b920f
brockclayton <brock@bluerockapps.com>
2019-09-16 06:10:59 -0600

    updated config

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 var config = {
+  // postgreSQL user, host, database and password
   database: {
-    user: 'fieldasset',
-    host: 'field-asset.crtnqmxucjr7.us-east-2.rds.amazonaws.com',
-    database: 'fieldasset',
-    password: 'fieldasset',
+    user: 'username',
+    host: 'hostname',
+    database: 'databasename',
+    password: 'databasepassword',
     port: 5432,
   },
   bluerockappsAPI: {
-    consumerSecret: 'supersecret',
+    consumerSecret: 'some-secret-string',
     tenantId: 'a0642972-e528-4071-b756-e103e85cd9f4'
   }
 };
